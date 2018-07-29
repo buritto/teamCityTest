1018b1f1748b0cb9af3ad8061dce50a4fe63d1e4
Arthur Karamow <ybiz177@gmail.com>
2018-07-29 10:46:13 +0600

    add class and tests

---
+++ ClassLibrary1/ClassLibrary1/IntOperations.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassLibrary1
+{
+    public class IntOperations
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Product(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ ClassLibrary1/Tests/TestsIntOperations.cs
@@ -0,0 +1,34 @@
+﻿using ClassLibrary1;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class TestsIntOperations
+    {
+        [Test]
+        public void Should_add_two_integer_number()
+        {
+            var operations = new IntOperations();
+            var firstNumber = 2;
+            var secondNumber = 3;
+
+            var expected = firstNumber + secondNumber;
+
+            operations.Add(firstNumber, secondNumber).Should().Be(expected);
+        }
+
+        [Test]
+        public void Should_product_two_integer()
+        {
+            var operations = new IntOperations();
+            var firstNumber = 3;
+            var secondNumber = 2;
+
+            var expected = firstNumber * secondNumber;
+            operations.Product(firstNumber, secondNumber).Should().Be(expected);
+        }
+        
+    }
+}
